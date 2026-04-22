72c93a2f5999d478d2183783ad5593fb0f6ec9e8
Stanislav Peresypkin <stas.peresypkin@mail.ru>
2026-04-22 16:04:30 +0300

    Added text input

--- obj/Debug/net8.0/DrowingTogether.AssemblyInfo.cs
+++ obj/Debug/net8.0/DrowingTogether.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DrowingTogether")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53620af06f70c333c4249329b89e02576cb8500c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41b799281985dcc8f8c937f1880e7b25c6e19e8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DrowingTogether")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DrowingTogether")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
